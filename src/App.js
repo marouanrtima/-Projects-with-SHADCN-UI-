2b23d46f0cc34e4b9f68ccd58d14ece58e99f8c8
marouan rtima <marouanrtima5@gmail.com>
2024-04-22 14:00:03 +0100

    insall SHADCN UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Button from "./components/ui/button";
+import {Button,buttonVariants} from "./components/ui/button";
 import "./App.css";
 
 function App() {
   return <div className="App">
     <Button variant={"default"}>Click me</Button>
+    <Button>Click me</Button>
     <Button>Click me</Button>
   </div>;
 }
